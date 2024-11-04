b9c5bfe1b082a2e35cfb317de65563393720b4e9
Sergio Arturo Pe?a Chaparro <sergioarturo9306@gmail.com>
2024-11-03 22:27:36 -0500

    ImprimirCara

---
+++ ImprimirCara.java
@@ -0,0 +1,10 @@
+public class ImprimirCara {
+    public static void main(String[] args) {
+        System.out.println("Imprimir la cara de Sergio.");
+        System.out.println(" @@@@@@@\r\n" + //
+                        "@| O O |@\r\n" + //
+                        "(|  ^  |)\r\n" + //
+                        " | [_] |\r\n" + //
+                        " +-----+");
+    }
+}
