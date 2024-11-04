95f970067f8c8ff9f434725fdea837695f2e84d4
Sergio Arturo Pe?a Chaparro <sergioarturo9306@gmail.com>
2024-11-03 22:33:25 -0500

    ImprimirDiamante

---
+++ ImprimirDiamante.java
@@ -0,0 +1,12 @@
+public class ImprimirDiamante {
+    public static void main(String[] args) {
+        System.out.println("Imprimir diamante");
+        System.out.println("   *\r\n" + //
+                        "  ***\r\n" + //
+                        " *****\r\n" + //
+                        "*******\r\n" + //
+                        " *****\r\n" + //
+                        "  ***\r\n" + //
+                        "   *");
+    }
+}
